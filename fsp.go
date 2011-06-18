f2020720682048bddc9aec0977fce79d4bf18b27
Jarod Liu <liuyuanzhi@gmail.com>
2011-06-18 11:27:37 +0800

    add ability to load policy from extenal file

--- fsp.go
+++ fsp.go
@@ -5,42 +5,55 @@ import (
   "bufio"
   "log"
 	"os"
+	"io"
+	"io/ioutil"
 )
 
 const (
-  ServePort = 843
+	DefaultPolicy = "<cross-domain-policy><allow-access-from domain=\"*\" to-ports=\"*\"/></cross-domain-policy>\x00"
 )
 
 type FspServer struct {
   listener *net.TCPListener 
+	policyContent string
 }
 
 func NewFspServer() *FspServer {
-  return &FspServer{}
+	fsp := &FspServer{}
+	fsp.policyContent = DefaultPolicy
+  return fsp
 }
 
 func (fs *FspServer) Startup() {
   addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:843")
   if (err != nil) {
-    log.Fatalf("net.ResolveTCPAddr: %s", err.String())
+    log.Fatalln(err.String())
   }
   fs.listener, err = net.ListenTCP("tcp", addr)
-  if (err != nil) {
-    log.Fatalf("net.ListenTCP: %s", err.String())
+  if err != nil {
+    log.Fatalln(err.String())
   }
 	log.Printf("Listening on %s", addr.String())
 
   for {
     conn, err := fs.listener.AcceptTCP()
 		if (err != nil) {
-			log.Fatalf("AcceptTCP: %s", err.String())
+			log.Fatalln(err.String())
 		}
-    go read(conn)
+    go fs.read(conn)
   }
   fs.listener.Close()
 }
 
-func read(conn *net.TCPConn) {
+func (fs *FspServer) LoadPolicy(src io.Reader) {
+	data, err := ioutil.ReadAll(bufio.NewReader(src))
+	if err != nil {
+		log.Fatalln(err.String())
+	}
+	fs.policyContent = string(data) + "\x00"
+}
+
+func (fs *FspServer) read(conn *net.TCPConn) {
 	defer conn.Close()
 
 	r := bufio.NewReader(conn)
@@ -49,12 +62,11 @@ func read(conn *net.TCPConn) {
 		if (err == os.EOF) {
 			return
 		} else if (err != nil) {
-			log.Fatalf("ReadString: %s", err.String())
+			log.Fatalln(err.String())
 		}
 
 		w := bufio.NewWriter(conn)
-		w.WriteString(`<cross-domain-policy><allow-access-from domain="*" to-ports="*"/></cross-domain-policy>`)
-		w.WriteString("\x00")
+		w.WriteString(fs.policyContent)
 		w.Flush()
 		log.Printf("Sent policy file to %s", conn.RemoteAddr().String())
 	}
--- main.go
+++ main.go
@@ -1,6 +1,37 @@
 package main
 
+import (
+	"flag"
+	"os"
+	"log"
+	"fmt"
+)
+
+const (
+	Version = "0.2"
+)
+
+var version = flag.Bool("version", false, "show gofsp version")
+var filename = flag.String("file", "", "policy file")
+
 func main() {
+	flag.Parse()
+
+	if *version {
+		fmt.Printf("gofsp - %s\n", Version)
+		os.Exit(0)
+	}
+
   fsp := NewFspServer()
+
+	// load policy from file
+	if len(*filename) > 0 {
+		file, err := os.Open(*filename)
+		if err != nil {
+			log.Fatalln(err.String())
+		}
+		fsp.LoadPolicy(file)
+	}
+
   fsp.Startup()
 }
